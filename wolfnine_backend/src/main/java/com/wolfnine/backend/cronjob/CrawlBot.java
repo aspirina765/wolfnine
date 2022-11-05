50a5c784eae9ab6474ddfc1bbb5e2ded053b6027
Jinner <nguyenhaidangyq@gmail.com>
2022-11-05 10:15:47 +0700

    Config Crawler Schedule: Update

--- wolfnine_backend/src/main/java/com/wolfnine/backend/cronjob/CrawlBot.java
+++ wolfnine_backend/src/main/java/com/wolfnine/backend/cronjob/CrawlBot.java
@@ -27,8 +27,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 @EnableAsync
-//@Component
-//@EnableScheduling
+@Component
+@EnableScheduling
 public class CrawlBot {
     private static ChromeOptions options;
     @Autowired
